34d684740e5a7f7f4d143fd1aba0345fd33a8a85
reid-cmyk <reid@highsman.com>
2026-03-30 16:17:37 -0400

    Update Layout.tsx

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,9 +1,11 @@
 import {Header} from './Header';
 import {Footer} from './Footer';
+import {AgeGate} from './AgeGate';
 
 export function Layout({children}: {children: React.ReactNode}) {
   return (
     <>
+      <AgeGate />
       <Header />
       <main className="pt-20">{children}</main>
       <Footer />
